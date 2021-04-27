9b7c014da1338d344832ff639a7976033a4716ce
Phil Iannuzzo <philiannuzzo@Mac-mini.asusrouter>
2021-04-27 00:53:29 -0400

    renamed video-icon.png and moved to icons folder

--- content.js
+++ content.js
@@ -1,8 +1,9 @@
 const { hostname, pathname } = document.location;
 
-const videoIconUrl = chrome.runtime.getURL("videoIcon.png");
 const carouselFrameUrl = chrome.runtime.getURL("carouselFrame.html");
 const videoTabUrl = chrome.runtime.getURL("/icons/icon-19.png");
+const videoIconUrl = chrome.runtime.getURL("/icons/video-icon.png");
 
 chrome.runtime.sendMessage({ greeting: "showPageIcon" });
+
 if (hostname === "baseball.fantasysports.yahoo.com") yahoo();
