ecc124994589c8e19fbb672f69e344be102ab457
Phil Iannuzzo <philiannuzzo@Mac-mini.asusrouter>
2021-06-27 01:01:04 -0400

    replaced XHR with fetch API in carousel.js

--- carousel.js
+++ carousel.js
@@ -1,38 +1,36 @@
-function getAnimationName() {
-	return $("#carousel").css("animation-name");
-}
-
 function exitCarousel() {
-	$("#carousel").css({
-		"animation-name": "exitCarousel",
-		"animation-duration": "300ms",
+	Object.assign(carousel.style, {
+		animationName: "exitCarousel",
+		animationDuration: "300ms",
 		bottom: "-265px",
 	});
 }
 
 function enterCarousel() {
-	$("#carousel").css({
-		"animation-name": "enterCarousel",
-		"animation-duration": "200ms",
+	Object.assign(carousel.style, {
+		animationName: "enterCarousel",
+		animationDuration: "200ms",
 		bottom: "0px",
 	});
 }
 
 function toggleCarousel() {
-	if (getAnimationName() === "enterCarousel") exitCarousel();
+	if (carousel.style.animationName === "enterCarousel") exitCarousel();
 	else enterCarousel();
 }
 
 async function insertCarousel() {
-	const response = await fetch(chrome.runtime.getURL("carousel.html"));
-	const carousel = await response.text();
-	$(carousel).appendTo("body");
+	const response = await fetch(carouselUrl);
+	const text = await response.text();
+	$(text).appendTo("body");
 	$("#fanlights").attr("src", videoTabUrl);
 	$("#videoTab").click(toggleCarousel);
 	$("#carouselFrame").attr("src", carouselFrameUrl);
-}
 
-chrome.runtime.onMessage.addListener(({ greeting, errorMessage }) => {
-	if (greeting === "enterCarousel") enterCarousel();
-	else if (greeting === "logError") console.error("[fanlights.io]", errorMessage);
-});
+	carousel = document.getElementById("carousel");
+
+	chrome.runtime.onMessage.addListener(({ greeting, errorMessage }) => {
+		if (greeting === "enterCarousel") return enterCarousel();
+		if (greeting === "logError") console.error("[fanlights.io]", errorMessage);
+	});
+}
