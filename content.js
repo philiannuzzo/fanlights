deb41633eeaf35f1ab4523bf1e4532e8f9533997
Phil Iannuzzo <phil.iannuzzo@gmail.com>
2024-05-05 22:39:07 -0400

    conversion from manifest-v2 to manifest-v3

--- content.js
+++ content.js
@@ -5,8 +5,6 @@ const carouselFrameUrl = chrome.runtime.getURL("carouselFrame.html");
 const videoTabUrl = chrome.runtime.getURL("/icons/icon-19.png");
 const videoIconUrl = chrome.runtime.getURL("/icons/video-icon.png");
 
-chrome.runtime.sendMessage({ greeting: "showPageIcon" });
-
 switch (hostname) {
 	case "baseball.fantasysports.yahoo.com":
 		yahoo();
